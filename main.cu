1dd2d8ea0c614b698cd225a76daf2f1340ead205
Kevin Le <2003kevinle@gmail.com>
2025-08-14 23:38:30 -0700

    added the ability to read from files rather than generate each time, and also added .txt files to .gitignore

--- main.cu
+++ main.cu
@@ -1,7 +1,50 @@
-
 #include "GEMM.cuh"
 #include "utils.cuh"
+#include <err.h>
+#include <format>
+#include <fstream>
 #include <iostream>
+
+void write_matrix(const float *matrix, int rows, int cols,
+                  std::string fileName) {
+  std::ofstream file(fileName);
+
+  if (!file.is_open()) {
+    std::cerr << "Error: file '" << fileName << "' could not be opened."
+              << std::endl;
+    return;
+  }
+
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      file << matrix[i * cols + j] << " ";
+    }
+    file << "\n";
+  }
+  file.close();
+}
+
+void read_file(float *matrix, int rows, int cols, std::string file_name) {
+  std::ifstream file(file_name);
+
+  if (!file.is_open()) {
+    std::cerr << "Error: Could not open file '" << file_name << "'"
+              << std::endl;
+    return;
+  }
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      if (!(file >> matrix[i * cols + j])) {
+        std::cerr << "Error: Unexpected end of file or invalid data."
+                  << std::endl;
+        // You can handle this error more gracefully, e.g., by returning a
+        // status code.
+        return;
+      }
+    }
+  }
+  file.close();
+}
 
 void initialize_matrices(int M, int K, int N, float *&h_A, float *&h_B,
                          float *&h_C) {
@@ -42,17 +85,25 @@ void gemm_cpu_reference(int M, int K, int N, const float *h_A, const float *h_B,
 }
 
 int main() {
-  const int M = 1024;
+  const int M = 4096;
   const int K = 2048;
-  const int N = 512;
+  const int N = 4096;
 
   float *h_A, *h_B, *h_C;
+  h_A = new float[M * K];
+  h_B = new float[K * N];
+  h_C = new float[M * N];
+  // ONLY DO THIS ON FIRST RUN
+  //  initialize_matrices(M, K, N, h_A, h_B, h_C);
+  //  write_matrix(h_A, M, K,"A_matrix.txt");
+  //  write_matrix(h_B, K, N, "B_matrix.txt");
+  //  gemm_cpu_reference(M, K, N, h_A, h_B, output_ref);
+  //  write_matrix(output_ref, M, N, "C_matrix.txt");
 
-  initialize_matrices(M, K, N, h_A, h_B, h_C);
   float *output_ref = new float[M * N];
-
-  gemm_cpu_reference(M, K, N, h_A, h_B, output_ref);
-
+  read_file(h_A, M, K, "A_matrix.txt");
+  read_file(h_B, K, N, "B_matrix.txt");
+  read_file(output_ref, M, N, "C_matrix.txt");
   // create a GEMM object
   GEMM *gemm = new GEMM(h_A, h_B, h_C, output_ref, M, N, K, BLOCK_SIZE);
   gemm->run_tests();
